d0c5c0e22952cb0b5708f986a4e93806a2d35793
raj-tati <mail.rajkumarreddy@gmail.com>
2024-07-04 07:34:43 +0530

    Update DSA.pl

--- DSA.pl
+++ DSA.pl
@@ -3,20 +3,32 @@ use feature qw(class);
 no warnings "experimental::class";
 
 class Stack {
-	my $array = [];
+	my $stack = [];
 	
-	method isEmpty()    { return(scalar(@{$array}) == 0); }
-	method push($value) { unshift(@{$array}, $value); }
-	method pop()        { return shift(@{$array}); }
-	method peek()       { return $array->[0]; }
-	method size()       { return scalar(@{$array}); }
+	method isEmpty()    { return(scalar(@{$stack}) == 0); }
+	method push($value) { unshift(@{$stack}, $value); }
+	method pop()        { return shift(@{$stack}); }
+	method peek()       { return $stack->[0]; }
+	method size()       { return scalar(@{$stack}); }
 }
 
+class Queue {
+	my $queue = [];
 
+	method isEmpty() 	   { return(scalar(@{$queue} == 0)); }
+	method enQueue($value) { unshift(@{$queue}, $value); }
+	method deQueue         { return shift(@{$queue}); }
+	method peek()          { return $queue->[0]; }
+	method size()		   { return scalar(@{$queue}); }
+}
 
 ## Testing DSA
 sub main {
 	my $stackObject = Stack->new();
 	$stackObject->push(5);
-	print($stackObject->pop()) if(!$stackObject->isEmpty);
+	print($stackObject->pop()) if(!$stackObject->isEmpty());
+
+	my $queueObject = Queue->new();
+	$queueObject->enQueue(10);
+	print($queueObject->deQueue()) if(!$queueObject->isEmpty());
 } main();
